1ab3cd2afe7d7af2a8b86ab258673dc4388ba73d
Kim TaeUk <140207345+sukple@users.noreply.github.com>
2023-07-24 05:37:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 143.38 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if i not in answer:
+            answer.append(i)
+        elif i != answer[-1]:
+            answer.append(i)
+            
+    return answer
