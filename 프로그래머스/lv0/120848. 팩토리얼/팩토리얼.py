3c4be81101c54e548a9c456e17482261209f5d14
Kim TaeUk <140207345+sukple@users.noreply.github.com>
2023-07-24 05:30:06 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,11 @@
+def solution(n):
+    a = 1
+    b= []
+    for i in range(1,11):
+        if a > n:
+            return i-2
+        a *= i
+        
+        print(6>6)
+    return i
+           
