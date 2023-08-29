84f1f9df5bdeaab152b26317b6624890ac97d61f
Kim TaeUk <140207345+sukple@users.noreply.github.com>
2023-08-29 21:13:36 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,6 @@
+def solution(my_string):
+    alpa = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    answer = [0]*52
+    for i in my_string:
+        answer[alpa.index(i)] += 1
+    return answer
