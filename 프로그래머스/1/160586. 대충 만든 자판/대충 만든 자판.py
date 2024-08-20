ba62d27d4d0a9783dcd7a435f8e1f2115ea001f0
Kim TaeUk <140207345+K-ple@users.noreply.github.com>
2024-08-20 16:26:01 +0900

    [level 1] Title: 대충 만든 자판, Time: 38.21 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,19 @@
+def solution(keymap, targets):
+    answer = []
+
+    for i in targets:
+        count = 0
+
+        for j in i:
+            idx = []
+            for k in keymap:
+                if j in k:
+                    idx.append(k.index(j)+1)
+                
+            
+            if not idx:
+                count = -1
+                break
+            count += min(idx)
+        answer.append(count)
+    return answer
